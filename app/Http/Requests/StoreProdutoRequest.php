dbb45bba43062cf2a4533b16a0f8538ab264b89e
Hiury Lima <limahiury6@gmail.com>
2025-05-16 17:03:57 -0300

    Add StoreProdutoRequest and UpdateProdutoRequest classes

---
+++ app/Http/Requests/StoreProdutoRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProdutoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|string|max:255',
+            'preco' => 'required|numeric|min:0.01',
+            'estoques' => 'required|array',
+            'estoques.*.variacao' => 'nullable|string|max:255',
+            'estoques.*.quantidade' => 'required|integer|min:0',
+        ];
+    }
+}
