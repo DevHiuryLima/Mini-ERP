318d0fd491a61359a11dc2740aeefb342cd067cc
Hiury Lima <limahiury6@gmail.com>
2025-05-17 10:06:48 -0300

    Fix "routes/web.php"

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::resource('produtos', ProdutoController::class);
