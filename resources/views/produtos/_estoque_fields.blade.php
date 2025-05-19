9ddc5721253c3532006054651e77b3b2002b0712
Hiury Lima <limahiury6@gmail.com>
2025-05-19 14:01:03 -0300

    Update view _estoque_fields.blade.php

--- resources/views/produtos/_estoque_fields.blade.php
+++ resources/views/produtos/_estoque_fields.blade.php
@@ -1,18 +1,18 @@
 <div class="row mb-3 estoque-item">
     <div class="col-md-5">
         <label class="form-label">Variação (opcional)</label>
-        <input type="text" 
+        <input type="text"
             name="estoques[{{ $index }}][variacao]"
             class="form-control"
             value="{{ old("estoques.$index.variacao", $item['variacao'] ?? '') }}">
     </div>
     <div class="col-md-5">
         <label class="form-label">Quantidade</label>
-        <input type="number" 
+        <input type="number"
             name="estoques[{{ $index }}][quantidade]"
             class="form-control"
             min="0"
-            value="{{ old("estoques.$index.quantidade", $item['quantidade'] ?? 0) }}"
+            value="{{ old("estoques.$index.quantidade", $item['quantidade'] ?? 1) }}"
             required>
         </div>
     <div class="col-md-2 d-flex align-items-end">
