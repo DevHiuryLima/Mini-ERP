cbc667faa50753cf3b6337bd035bb39022d1dbe5
Hiury Lima <limahiury6@gmail.com>
2025-05-20 00:57:05 -0300

    Add StoreCupomRequest and UpdateCupomRequest classes

---
+++ app/Http/Requests/StoreCupomRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCupomRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'codigo' => 'required|string|unique:cupons,codigo|max:50',
+            'tipo_desconto' => 'required|in:percentual,fixo',
+            'valor' => 'required|numeric|min:0.01',
+            'minimo_subtotal' => 'required|numeric|min:0',
+            'validade' => 'required|date|after_or_equal:today',
+            'ativo' => 'sometimes|boolean',
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdateCupomRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCupomRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $id = $this->route('cupom')->id;
+        return [
+            'codigo'         => "required|string|unique:cupons,codigo,{$id}|max:50",
+            'tipo_desconto'  => 'required|in:percentual,fixo',
+            'valor'          => 'required|numeric|min:0.01',
+            'minimo_subtotal'=> 'required|numeric|min:0',
+            'validade'       => 'required|date|after_or_equal:today',
+            'ativo'          => 'sometimes|boolean',
+        ];
+    }
+}
